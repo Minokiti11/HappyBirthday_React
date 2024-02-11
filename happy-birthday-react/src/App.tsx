cea7d7ac50c6d3bb733ef63b0d14e13a04e780e0
Mino-rex <macminosp@icloud.com>
2024-02-11 17:45:18 +0900

    Update App.tsx

--- happy-birthday-react/src/App.tsx
+++ happy-birthday-react/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import LinkButton from "./children/link";
+import "./App.css";
+// import LinkButton from "./children/link";
 
 function App() {
   const title: string = "Happy Birthday!!";
   const [num, setNum] = useState(0);
-
+  
   const increment = () => {
     setNum(num + 1);
     console.log(num);
@@ -19,14 +20,52 @@ function App() {
     <div className="App">
       <h1>{title}</h1>
       Your Age: {num}
-      {/* <LinkButton text="+" link="/test" /> */}
       <button type="button" onClick={increment}>
         +
       </button>
       <button type="button" onClick={decrement}>
         -
       </button>
-      <p><img src={`${process.env.PUBLIC_URL}/cake.png`}></img></p>
+      <div className="cake-body">
+        <div className="layer f1">
+          <div className="spot-group sg1">
+            <div className="spot s1"></div>
+            <div className="spot s2"></div>
+            <div className="spot s3"></div>
+          </div>
+          <div className="spot-group sg2">
+            <div className="spot s1"></div>
+            <div className="spot s2"></div>
+          </div>
+        </div>
+        <div className="layer f2">
+          <div className="spot-group sg3">
+            <div className="spot s1"></div>
+            <div className="spot s2"></div>
+          </div>
+          <div className="spot-group sg4">
+            <div className="spot s1"></div>
+            <div className="spot s2"></div>
+            <div className="spot s3"></div>
+          </div>
+        </div>
+        <div className="layer f3">
+          <div className="spot-group sg5">
+              <div className="spot s1"></div>
+              <div className="spot s2"></div>
+              <div className="spot s3"></div>
+          </div>
+        </div>
+        <div className="layer f4"></div>
+        <div className="candle-ctn">
+            <div className="candle c1">
+              <div className="c-base"></div>
+              <div className="wick"></div>
+              <div className="flame"></div>
+              <div className="highlight"></div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ happy-birthday-react/src/candle.tsx
@@ -0,0 +1,8 @@
+import { createElement } from 'react';
+
+function PlantingCandle({ }) {
+  return createElement(
+    'div',
+    { className: 'candle' },
+  );
+}
